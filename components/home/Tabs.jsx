add26b1818ab17dddefdbb10634fd7c71e2734fa
Sien Khumaen Damarwendha <sienkhumaen@gmail.com>
2023-12-01 06:20:27 +0700

    🤏 fix some changes

--- components/home/Tabs.jsx
+++ components/home/Tabs.jsx
@@ -1,19 +1,19 @@
 function Tabs() {
   return (
-    <div class="text-sm font-medium text-center text-gray-500 border-b border-gray-200 bg-classic-black">
-      <ul class="flex flex-wrap -mb-px">
-        <li class="me-2">
+    <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 bg-classic-black">
+      <ul className="flex flex-wrap -mb-px">
+        <li className="me-2">
           <a
             href="#"
-            class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+            className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
           >
             All courses
           </a>
         </li>
-        <li class="me-2">
+        <li className="me-2">
           <a
             href="#"
-            class="inline-block p-4 text-classic-white border-b-2 border-classic-white rounded-t-lg active "
+            className="inline-block p-4 border-b-2 rounded-t-lg text-classic-white border-classic-white active "
             aria-current="page"
           >
             My lists
--- components/navbar/MainNav.jsx
+++ components/navbar/MainNav.jsx
@@ -13,19 +13,31 @@ function Navbar() {
   return (
     <NavbarUi fluid className="relative z-50 drop-shadow-md">
       <NavbarBrand as={Link} href="/">
-        <Logo width={35} height={35} className="mr-1" />
+        <Logo size="md" className="mr-2" />
         <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-classic-black">
           Taxacademy
         </span>
       </NavbarBrand>
       <NavbarToggle />
-      <div className="absolute left-0 top-14 border-t-2 sm:border-t-0 z-50 flex flex-col w-full bg-classic-black sm:w-auto sm:bg-transparent sm:static sm:gap-10 sm:flex-row">
+      <div className="absolute left-0 z-20 flex flex-col w-full top-14 sm:border-t-0 bg-classic-black sm:w-auto sm:bg-transparent sm:static sm:gap-10 sm:flex-row">
         <NavbarCollapse>
-          <NavbarLink as={Link} href="#" className="border-y-0 hover:bg-classic-black sm:-mr-6">
-            <Button className="center-this sm:text-classic-black sm:border-gray-400 rounded-none">Log in</Button>
+          <NavbarLink
+            as={Link}
+            href="#"
+            className="border-y-0 hover:bg-classic-black sm:-mr-6"
+          >
+            <Button className="rounded-none center-this sm:text-classic-black sm:border-gray-400">
+              Log in
+            </Button>
           </NavbarLink>
-          <NavbarLink as={Link} href="#" className="border-y-0 bg-brand-primary hover:bg-purple-700">
-            <Button className="center-this sm:hover:bg-brand-primary rounded-none">Sign up</Button>
+          <NavbarLink
+            as={Link}
+            href="#"
+            className="border-y-0 bg-brand-primary hover:bg-purple-700"
+          >
+            <Button className="rounded-none center-this sm:hover:bg-brand-primary">
+              Sign up
+            </Button>
           </NavbarLink>
         </NavbarCollapse>
       </div>
