ff96d11f71329be7ab86a642d6d4487020a4ff65
Sien Khumaen Damarwendha <sienkhumaen@gmail.com>
2023-12-01 00:11:55 +0700

    🗽 feat(navbar): add navbar contents

--- components/navbar/MainNav.jsx
+++ components/navbar/MainNav.jsx
@@ -5,12 +5,13 @@ import {
   NavbarLink,
   NavbarCollapse,
   NavbarToggle,
+  Button,
 } from "flowbite-react";
 import Logo from "../Logo";
 
 function Navbar() {
   return (
-    <NavbarUi fluid className="relative drop-shadow-md">
+    <NavbarUi fluid className="relative z-50 drop-shadow-md">
       <NavbarBrand as={Link} href="/">
         <Logo width={35} height={35} className="mr-1" />
         <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-classic-black">
@@ -18,13 +19,13 @@ function Navbar() {
         </span>
       </NavbarBrand>
       <NavbarToggle />
-      <div className="absolute left-0 z-40 flex flex-col w-full bg-grey top-16 sm:w-auto sm:bg-transparent sm:static sm:gap-10 sm:flex-row">
+      <div className="absolute left-0 top-14 border-t-2 sm:border-t-0 z-50 flex flex-col w-full bg-classic-black sm:w-auto sm:bg-transparent sm:static sm:gap-10 sm:flex-row">
         <NavbarCollapse>
-          <NavbarLink as={Link} href="#" active className="text-center">
-            Home
+          <NavbarLink as={Link} href="#" className="border-y-0 hover:bg-classic-black sm:-mr-6">
+            <Button className="center-this sm:text-classic-black sm:border-gray-400 rounded-none">Log in</Button>
           </NavbarLink>
-          <NavbarLink as={Link} href="#" className="text-center">
-            About
+          <NavbarLink as={Link} href="#" className="border-y-0 bg-brand-primary hover:bg-purple-700">
+            <Button className="center-this sm:hover:bg-brand-primary rounded-none">Sign up</Button>
           </NavbarLink>
         </NavbarCollapse>
       </div>
