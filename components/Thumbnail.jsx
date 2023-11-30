cb7e5b7a111ce257008e7b061ab1b621c8bf1835
Sien Khumaen Damarwendha <sienkhumaen@gmail.com>
2023-12-01 02:17:07 +0700

    🤏 chore(Thumbnail): fix bug

--- components/Thumbnail.jsx
+++ components/Thumbnail.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function Thumbnail({ src, alt = "Thumbnail", ...props }) {
   return (
-    <span className="relative w-full max-w-md aspect-video sm:mr-8">
+    <span className="flex relative w-full max-w-md aspect-video sm:mr-8">
       <Image src={src} alt={alt} {...props} fill />;
     </span>
   );
