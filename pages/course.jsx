dfb2ab66d53e3fb7e141dee41ce54304c174178f
Sien Khumaen Damarwendha <sienkhumaen@gmail.com>
2023-12-02 12:36:30 +0700

    🤏 chore(course): add MainNav into course page

--- pages/course.jsx
+++ pages/course.jsx
@@ -1,11 +1,11 @@
-"use client";
-
 import Hero from "@/components/course/Hero";
 import Main from "@/components/course/Main";
+import MainNav from "@/components/navbar/MainNav";
 
 function Course() {
   return (
     <>
+      <MainNav />
       <Hero />
       <Main />
     </>
