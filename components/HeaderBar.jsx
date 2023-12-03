a27e12a269430f65df209349c7a7971d617b75ee
Sien Khumaen Damarwendha <sienkhumaen@gmail.com>
2023-12-03 13:47:46 +0700

    fix error

--- components/HeaderBar.jsx
+++ components/HeaderBar.jsx
@@ -1,11 +1,11 @@
-import Link from "next/link";
+import Link from "./Link";
 import { FaChevronLeft } from "react-icons/fa6";
 
 function HeaderBar({ href, title, onClick }) {
   return (
     <div className="flex items-center w-screen font-semibold text-gray-200 border-b-2 border-b-brand-primary h-11 text-md bg-classic-black text-classic-white">
       <Link
-        className="px-4 text-3xl text-gray-200"
+        className="p-4 text-3xl text-gray-200"
         href={href}
         onClick={onClick}
       >
